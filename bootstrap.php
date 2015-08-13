7ebdc3fe73314d9d875f4f6c64be002d554e6311
ricardo <ricardo@debian>
2015-08-13 10:23:23 -0300

    Organização dos arquivos de bootstrap do modulo

--- bootstrap.php
+++ bootstrap.php
@@ -1,36 +1,3 @@
 <?php 
 
-namespace CineFavela\Usuario;
-
-class Module extends \CineFavela\Core\Module\Module
-{
-
-    public function __construct()
-    {
-        parent::__construct("usuario", "0.1.0");
-    }
-
-    public function getTemplateConfig()
-    {
-        return array(
-            "templateDir" => array(
-                APPLICATION_PATH . "/modules/" . ucfirst($this->getName()) . "/template/"
-            )
-        );
-    }
-
-    public function getRouteConfig()
-    {
-        return array(
-            "routes" => array(
-                array(
-                    "method" => "get",
-                    "path" => "/usuario",
-                    "controller" => "CineFavela\\Usuario\\Controllers\\UsuarioController"
-                )
-            )
-        );
-    }
-}
-
 \CineFavela\Core\Application::getModuleManager()->register(new \CineFavela\Usuario\Module());
---
+++ src/Module.php
@@ -0,0 +1,34 @@
+<?php 
+
+namespace CineFavela\Usuario;
+
+class Module extends \CineFavela\Core\Module\Module
+{
+
+    public function __construct()
+    {
+        parent::__construct("usuario", "0.1.0");
+    }
+
+    public function getTemplateConfig()
+    {
+        return array(
+            "templateDir" => array(
+                dirname(realpath(__FILE__)) . "/../template/"
+            )
+        );
+    }
+
+    public function getRouteConfig()
+    {
+        return array(
+            "routes" => array(
+                array(
+                    "method" => "get",
+                    "path" => "/usuario",
+                    "controller" => "CineFavela\\Usuario\\Controllers\\UsuarioController"
+                )
+            )
+        );
+    }
+}
